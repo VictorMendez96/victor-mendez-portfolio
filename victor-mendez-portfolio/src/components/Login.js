30a5ef41da6f0ffb5ce29c8ced5cd3b68b821e01
Victor Mendez <victormendez990@gmail.com>
2022-11-15 02:09:59 -0700

    Create Login/Signup components

---
+++ victor-mendez-portfolio/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LogInForm () {
+    return (
+        
+    )
+};
+
+export default LogInForm;
---
+++ victor-mendez-portfolio/src/components/SignUp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SignUpForm () {
+    return (
+
+    )
+};
+
+export default SignUpForm;
